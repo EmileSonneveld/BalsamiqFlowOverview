7da436a27c965d8dc175f9c2addbcf4d015695dc
Emile Sonneveld <contact@emilesonneveld.be>
2019-01-03 21:13:50 +0100

    SQLite

--- BalsamiqFlowOverview/Program.cs
+++ BalsamiqFlowOverview/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SQLite;
 
 namespace BalsamiqFlowOverview
 {
@@ -10,6 +11,9 @@ namespace BalsamiqFlowOverview
 	{
 		static void Main(string[] args)
 		{
+			var m_dbConnection =
+new SQLiteConnection("Data Source=C:/Users/emill/Dropbox (Persoonlijk)/slimmerWorden/2018-2019-Semester1/CMDM/PROJECT/balsamiq_mockups.bmpr;Version=3;");
+			m_dbConnection.Open();
 		}
 	}
 }
