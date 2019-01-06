bf9725b81d05a3380780f9042998086235c1c67d
Emile Sonneveld <contact@emilesonneveld.be>
2019-01-06 15:00:47 +0100

    Also generate graph with node. Don't output own SVG experiment.

--- BalsamiqFlowOverview.UnitTestProject/UnitTest1.cs
+++ BalsamiqFlowOverview.UnitTestProject/UnitTest1.cs
@@ -19,7 +19,51 @@ namespace BalsamiqFlowOverview.UnitTestProject
 			var href = parsed.mockup.controls.control[0].properties.hrefs.href[1].ID;
 			Guid.Parse(href); // Should not crash
 			Debug.Assert(href == "33915D60-C70E-A976-E11B-791060D6A925");
+		}
 
+		/*[TestMethod]
+		public void TestMethod2()
+		{
+			object ret = Program.EvalJScript("1+2");
+			Debug.Assert(ret.Equals(3));
+		}*/
+
+
+		[TestMethod]
+		public void TestMethod2()
+		{
+			var svg = Program.GraphVizToSvg(@"digraph BalsamiqFlowOverview {
+	rankdir=LR;
+	node [shape = rectangle];
+	""patient_make_appointment""->""patient_make_appointment_online""[label = ""Make appointment on this date""]
+	""patient_make_appointment_online""->""information_submitted""[label = ""Submit""]
+	""patient_make_appointment_online""->""select_doctor""[label = ""Select doctor""]
+	""patient_make_appointment_online""->""patient_portal""[label = ""cancel""]
+	""admin_modify_appointment""->""admin_search_patient""[label = ""Set patient""]
+	""admin_modify_appointment""->""information_submitted""[label = ""Done""]
+	""admin_modify_appointment""->""select_doctor""[label = ""Set doctor""]
+	""patient_register""->""account_registered""[label = ""Register""]
+	""account_registered""->""after_confirmation_mail""[label = ""click confirmation link""]
+	""after_confirmation_mail""->""patient_login""[label = ""Go to login""]
+	""patient_login""->""patient_portal""[label = ""Log in""]
+	""patient_portal""->""patient_make_appointment""[label = ""Make appointment""]
+	""patient_portal""->""patient_view_medical_file""[label = ""View my medical file""]
+	""patient_portal""->""patient_make_appointment""[label = ""View schedule""]
+	""patient_portal""->""vaccinations_needed_for_country""[label = ""View vaccinations needed for country""]
+	""patient_view_medical_file""->""patient_portal""[label = ""<- Back""]
+	""patient_view_schedule""->""patient_make_appointment_online""[label = ""Edit this appointment""]
+	""doctor_portal""->""admin_search_patient""[label = ""Search patients""]
+	""doctor_portal""->""doctor_view_doctor_schedule""[label = ""See Schedule""]
+	""admin_search_patient""->""doctor_patient_view_medical_file""[label = ""View patient information""]
+	""admin_search_patient""->""select_doctor""[label = ""Linked to doctor""]
+	""doctor_patient_view_medical_file""->""doctor_portal""[label = ""<- Back""]
+	""doctor_view_patient_schedule""->""admin_modify_appointment""[label = ""Edit this appointment""]
+	""doctor_view_patient_schedule""->""doctor_portal""[label = ""<- Back""]
+	""doctor_view_doctor_schedule""->""admin_modify_appointment""[label = ""Edit this appointment""]
+}
+");
+			Debug.Assert(svg.ToLower().Contains("<svg"));
+			Debug.Write(svg);
 		}
 	}
 }
--- BalsamiqFlowOverview/Program.cs
+++ BalsamiqFlowOverview/Program.cs
@@ -10,7 +10,7 @@ using System.IO;
 
 namespace BalsamiqFlowOverview
 {
-	static class Program
+	public static class Program
 	{
 		static string imagerUlPrefix = "data:image/png;base64,";
 
@@ -54,32 +54,38 @@ namespace BalsamiqFlowOverview
 				var attr = attrs[pair.Key];
 
 				var lst = new List<FlowLink>();
-				var cts = GetControls(pair.Value);
-				foreach (var c in cts)
+				if (!attr.trashed)
 				{
-					var controlText = c.properties?.text;
-					foreach (var href in GetHrefs(c))
+					var cts = GetControls(pair.Value);
+					foreach (var c in cts)
 					{
-						var fl = new FlowLink();
-						fl.linkName = controlText;
-						fl.screen = flowScreens[href];
-						lst.Add(fl);
+						var controlText = c.properties?.text;
+						foreach (var href in GetHrefs(c))
+						{
+							var fl = new FlowLink();
+							fl.linkName = controlText;
+							fl.screen = flowScreens[href];
+							lst.Add(fl);
+						}
 					}
 				}
-
 				flowScreens[pair.Key].linksToScreens = lst;
 
 			}
 
 			var flowOverview = new FlowOverview(flowScreens.Values.ToList());
 			flowOverview.CalculateLayout();
-			var svg = flowOverview.GetSvg();
-			Console.WriteLine("Outputting flow.svg");
-			File.WriteAllText("flow.svg", svg);
+			//var svg = flowOverview.GetSvg();
+			//Console.WriteLine("Outputting flow.svg");
+			//File.WriteAllText("flow.svg", svg);
 
 			var graphViz = flowOverview.GetGraphViz();
 			Console.WriteLine("Outputting flow.txt");
 			File.WriteAllText("flow.txt", graphViz);
+
+			var svg2 = GraphVizToSvg(graphViz);
+			Console.WriteLine("Outputting flow_graph.svg");
+			File.WriteAllText("flow_graph.svg", svg2);
 			//Console.ReadLine();
 		}
 
@@ -95,7 +101,7 @@ namespace BalsamiqFlowOverview
 
 			if (!string.IsNullOrEmpty(c.properties?.href?.ID))
 				ret.Add(c.properties.href.ID);
-			
+
 			if (c.properties?.hrefs?.href != null)
 			{
 				foreach (var href in c.properties.hrefs.href)
@@ -107,6 +113,44 @@ namespace BalsamiqFlowOverview
 			return ret;
 		}
 
+		public static string GraphVizToSvg(string graphCode)
+		{
+			try
+			{
+				var vizCode = File.ReadAllText("../../../BalsamiqFlowOverview/viz.js");
+				vizCode += @"
+var data = `" + graphCode + @"`;
+var svg = this.Viz(data, 'svg');
+console.log(svg)
+";
+				string fileName = System.IO.Path.GetTempPath() + Guid.NewGuid().ToString() + ".js";
+				File.WriteAllText(fileName, vizCode);
+
+				// Start the child process.
+				Process p = new Process();
+				// Redirect the output stream of the child process.
+				p.StartInfo.UseShellExecute = false;
+				p.StartInfo.RedirectStandardOutput = true;
+				p.StartInfo.FileName = "node";
+				p.StartInfo.Arguments = fileName;
+				p.Start();
+				// Do not wait for the child process to exit before
+				// reading to the end of its redirected stream.
+				// p.WaitForExit();
+				// Read the output stream first and then wait.
+				string output = p.StandardOutput.ReadToEnd();
+				p.WaitForExit();
+				return output;
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message + "\n\n");
+				var str = "!Could not run node to generate the graph! Be sure that it is installed, and added to path.\n";
+				str += "The graph code is still exported and can be copy pasted into http://webgraphviz.com";
+				Console.WriteLine(str);
+				return "<svg>" + str + "</svg>";
+			}
+		}
 
 	}
 }
